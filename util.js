910e53fd1a950c59bf794882fa583bb3958e7696
rht0910 <rht0910@outlook.jp>
2018-07-23 00:27:28 +0900

    remove comments

--- util.js
+++ util.js
@@ -1,17 +1,17 @@
-const fs = require('fs').promises; // <- Cannot find module fs/promises [fixed]
+const fs = require('fs').promises;
 
 module.exports = {
   async exists(path) {
     return await fs.access(path).then(() => true).catch(() => false)
   },
   async initJSON(path, json) {
-    if (await this.exists(path)) return // <- exists is not defined (x3) [fixed] but.............
+    if (await this.exists(path)) return
     console.log(`Creating ${path}`);
     return await this.writeJSON(path, json)
   },
-  async readJSON(path, default2) { // <- Unexpected token `default` [fixed]
+  async readJSON(path, default2) {
     const data = await fs.readFile(path, 'utf8').catch(() => {})
-    const json = data ? this.parse(data) : default2 // <- Unexpected token `default` [fixed]
+    const json = data ? this.parse(data) : default2
     // TODO: 例外必要？
     return this.parse(data)
   },
