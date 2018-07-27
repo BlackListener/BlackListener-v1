f8c8a48aac285522cb6e1f1e713e9b2f39b55c28
yuta0801 <yuta20030801@icloud.com>
2018-07-28 00:24:05 +0900

    Return error when no default value is given on util.readJSON() method

--- util.js
+++ util.js
@@ -10,8 +10,9 @@ module.exports = {
     return await this.writeJSON(path, json)
   },
   async readJSON(path, _default) {
-    const data = await fs.readFile(path, 'utf8').catch(() => {})
-    return data ? this.parse(data) : _default
+    return await fs.readFile(path, 'utf8')
+      .then(data => this.parse(data))
+      .catch(err => _default ? null : err)
   },
   async writeJSON(path, json) {
     const data = this.stringify(json)
