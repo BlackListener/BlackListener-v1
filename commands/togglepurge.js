6f10dc242240b250ad4a6c16f5784ac6beac5d01
rht0910 <rht0910@rht0910.tk>
2018-09-05 19:15:13 +0900

    is togglepurge easy to read?

--- commands/togglepurge.js
+++ commands/togglepurge.js
@@ -9,18 +9,16 @@ module.exports.isAllowed = msg => {
 
 module.exports.run = async function(msg, settings, lang, guildSettings) {
   const args = msg.content.replace(settings.prefix, '').split(' ')
-  const unsavedSettings = settings
-  if (args[1] === 'enable') {
-    unsavedSettings.disable_purge = false
-  } else if (args[1] === 'disable') {
-    unsavedSettings.disable_purge = true
-  } else {
-    if (settings.disable_purge) {
-      unsavedSettings.disable_purge = false
-    } else if (!settings.disable_purge) {
-      unsavedSettings.disable_purge = true
-    }
+  if (args[1] === 'enable')
+    settings.disable_purge = false
+  else if (args[1] === 'disable')
+    settings.disable_purge = true
+  else {
+    if (settings.disable_purge)
+      settings.disable_purge = false
+    else
+      settings.disable_purge = true
   }
-  cs.store(guildSettings, unsavedSettings)
+  cs.store(guildSettings, settings)
   msg.channel.send(f(lang.setconfig, 'disable_purge'))
 }
