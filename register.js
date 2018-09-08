4e5a0727f8c753116192192dda9c9435d424abb0
yuta0801 <yuta20030801@icloud.com>
2018-09-08 17:56:40 +0900

    Clean up register.js

--- register.js
+++ register.js
@@ -8,6 +8,61 @@ global.loadConfig = function() {
 }
 
 const codeblock = code => '```' + code + '```'
+const ucfirst = text => text.charAt(0).toUpperCase() + text.slice(1)
+
+function makeReport(client, error, type) {
+  const date = new Date()
+  const description = type === 'error'
+    ? 'Unhandled Rejection(Exception/Error in Promise).'
+    : 'Uncaught error.'
+  const format = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}`
+  const argv = process.argv.map((val, index) => `arguments[${index}]: ${val}`)
+  const data =  `
+--- BlackListener ${ucfirst(type)} Report ---
+
+Time: ${format}
+Description: ${description}
+
+${error.stack}
+
+--- Process Details ---
+    Last Called Logger Thread: ${global.thread} (may not current thread)
+
+    BlackListener Version: ${c.version}
+    BlackListener Build: ${c.build}
+
+    Arguments: ${process.argv.length}
+    ${argv.join('\n    ')}
+
+    Launched in PID: ${process.pid}
+
+    Remote control: ${process.env.ENABLE_RCON ? 'Enabled' : 'Disabled'}
+    Custom Prefix: ${process.env.BL_PREFIX || 'Disabled; using default value: '+c.prefix}
+
+--- Discord.js ---
+    Average ping of websocket: ${Math.floor(client.ping * 100) / 100}
+    Last ping of websocket: ${client.pings[0]}
+    Ready at: ${client.readyAt.toLocaleString()}
+
+--- System Details ---
+    CPU Architecture: ${process.arch}
+    Platform: ${process.platform}
+    Total Memory: ${Math.round(os.totalmem() / 1024 / 1024 * 100) / 100}MB
+    Memory Usage: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100}MB
+
+--- Versions ---
+    Node Version: ${process.versions.node}
+    HTTP Parser Version: ${process.versions.http_parser}
+    v8 Version: ${process.versions.v8}
+    Unicode Version: ${process.versions.unicode}
+    zlib Version: ${process.versions.zlib}
+    OpenSSL Version: ${process.versions.openssl}
+`
+  return {
+    report: data,
+    file: `./${type}-reports/${type}-${format}.txt`,
+  }
+}
 
 module.exports = function(client) {
   let count = 0
@@ -28,114 +83,20 @@ module.exports = function(client) {
 
   process.on('unhandledRejection', (error) => {
     if (error.name === 'DiscordAPIError') return true // if DiscordAPIError, just ignore it(e.g. Missing Permissions)
-    const date = new Date()
-    const format = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}`
-    const file = `./error-reports/error-${format}.txt`
-    let arguments = ''
-    process.argv.forEach((val, index) => {
-      arguments += `    arguments[${index}]: ${val}\n`
-    })
-    const data = `
---- BlackListener Error Report ---
-
-Time: ${format}
-Description: Unhandled Rejection(Exception/Error in Promise).
-
-${error.stack}
-
---- Process Details ---
-    Last Called Logger Thread: ${global.thread} (may not current thread)
-
-    BlackListener Version: ${c.version}
-    BlackListener Build: ${c.build}
-
-    Arguments: ${process.argv.length}
-${arguments}
-
-    Launched in PID: ${process.pid}
-
-    Remote control: ${process.env.ENABLE_RCON ? 'Enabled' : 'Disabled'}
-    Custom Prefix: ${process.env.BL_PREFIX || 'Disabled; using default value: '+c.prefix}
-
---- Discord.js ---
-    Average ping of websocket: ${global.client.ping}
-    Last ping of websocket: ${global.client.pings[0]}
-    Ready at: ${global.client.readyAt.toLocaleString()}
-
---- System Details ---
-    CPU Architecture: ${process.arch}
-    Platform: ${process.platform}
-    Total Memory: ${Math.round(os.totalmem() / 1024 / 1024 * 100) / 100}MB
-    Memory Usage: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100}MB
-
---- Versions ---
-    Node Version: ${process.versions.node}
-    HTTP Parser Version: ${process.versions.http_parser}
-    v8 Version: ${process.versions.v8}
-    Unicode Version: ${process.versions.unicode}
-    zlib Version: ${process.versions.zlib}
-    OpenSSL Version: ${process.versions.openssl}
-`
-    global.client.channels.get('484357084037513216').send(codeblock(data))
+    const { report, file } = makeReport(client, error, 'error')
+    client.channels.get('484357084037513216').send(codeblock(report))
       .then(() => logger.info('Error report has been sent!'))
     logger.error(`Unhandled Rejection: ${error}`)
     logger.error(error.stack)
-    fs.writeFile(file, data, 'utf8').then(() => {
+    fs.writeFile(file, report, 'utf8').then(() => {
       logger.info(`Error Report has been writed to ${file}`)
     })
   })
 
   process.on('uncaughtException', (error) => {
-    const date = new Date()
-    const format = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}`
-    const file = `./crash-reports/crash-${format}.txt`
-    let arguments = ''
-    process.argv.forEach((val, index) => {
-      arguments += `    arguments[${index}]: ${val}\n`
-    })
-    const data = `
---- BlackListener Crash Report ---
-
-Time: ${format}
-Description: Uncaught error.
-
-${error.stack}
-
---- Process Details ---
-    Last Called Logger Thread: ${global.thread} (may not current thread)
-
-    BlackListener Version: ${c.version}
-    BlackListener Build: ${c.build}
-
-    Arguments: ${process.argv.length}
-${arguments}
-
-    Launched in PID: ${process.pid}
-
-    Remote control: ${process.env.ENABLE_RCON ? 'Enabled' : 'Disabled'}
-    Custom Prefix: ${process.env.BL_PREFIX || 'Disabled; using default value: '+c.prefix}
-
---- Discord.js ---
-    Average ping of websocket: ${Math.floor(global.client.ping * 100) / 100}
-    Last ping of websocket: ${global.client.pings[0]}
-    Ready at: ${global.client.readyAt.toLocaleString()}
-
---- System Details ---
-    CPU Architecture: ${process.arch}
-    Platform: ${process.platform}
-    Total Memory: ${Math.round(os.totalmem() / 1024 / 1024 * 100) / 100}MB
-    Memory Usage: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100}MB
-
---- Versions ---
-    Node Version: ${process.versions.node}
-    HTTP Parser Version: ${process.versions.http_parser}
-    v8 Version: ${process.versions.v8}
-    Unicode Version: ${process.versions.unicode}
-    zlib Version: ${process.versions.zlib}
-    OpenSSL Version: ${process.versions.openssl}
-`
-    require('fs').writeFileSync(file, data, 'utf8')
-    global.client.channels.get('484183865976553493').send(codeblock(data))
+    const { report, file } = makeReport(client, error, 'crash')
+    require('fs').writeFileSync(file, report, 'utf8')
+    client.channels.get('484183865976553493').send(codeblock(report))
       .then(() => process.exit(1))
   })
 
