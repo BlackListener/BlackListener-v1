71acaef7152238f413d0d4073aa7c0bb7c271770
rht0910 <rht0910@rht0910.tk>
2018-09-12 12:17:55 +0900

    removed localSettings

--- commands/antispam.js
+++ commands/antispam.js
@@ -21,9 +21,8 @@ module.exports.run = async function(msg, settings, lang, guildSettings) {
   const args = msg.content.replace(settings.prefix, '').split(' ')
   const command = `${settings.prefix}antispam`
   const write = async function(value) {
-    const localSettings = settings
-    localSettings.antispam = value
-    await util.writeSettings(guildSettings, localSettings, null, null, false)
+    settings.antispam = value
+    await util.writeSettings(guildSettings, settings, null, null, false)
   }
   if (!args[1] || args[1] === 'help') {
     const status = settings.antispam ? lang.enabled : lang.disabled
@@ -49,19 +48,18 @@ module.exports.run = async function(msg, settings, lang, guildSettings) {
   } else if (args[1] === 'ignore') {
     if (!msg.mentions.channels.first()) { return msg.channel.send(lang.invalid_args) }
     if (/\s/.test(args[2]) || !args[2]) { return msg.channel.send(lang.cannotspace) }
-    const localSettings = settings
     let user2 = msg.mentions.channels.first()
     if (!user2) user2 = msg.guild.channels.find(n => n.name === args[2])
     if (!user2) user2 = msg.guild.channels.get(args[2])
     const id = user2 ? user2.id : ':poop:'
     if (id === ':poop:') return msg.channel.send(lang.invalid_args)
-    if (localSettings.ignoredChannels.includes(id)) {
-      localSettings.ignoredChannels.splice(localSettings.ignoredChannels.indexOf(id), 1)
-      await util.writeSettings(guildSettings, localSettings, null, null, false)
+    if (settings.ignoredChannels.includes(id)) {
+      settings.ignoredChannels.splice(settings.ignoredChannels.indexOf(id), 1)
+      await util.writeSettings(guildSettings, settings, null, null, false)
       msg.channel.send(lang.antispam.ignore_enabled)
     } else {
-      localSettings.ignoredChannels.push(id)
-      await util.writeSettings(guildSettings, localSettings, null, null, false)
+      settings.ignoredChannels.push(id)
+      await util.writeSettings(guildSettings, settings, null, null, false)
       msg.channel.send(lang.antispam.ignore_disabled)
     }
   } else if (args[1] === 'status') {
--- commands/autorole.js
+++ commands/autorole.js
@@ -13,13 +13,11 @@ module.exports.isAllowed = msg => {
 module.exports.run = async function(msg, settings, lang, guildSettings) {
   const args = msg.content.replace(settings.prefix, '').split(' ')
   if (args[1] === 'remove') {
-    const localSettings = settings
-    localSettings.autorole = null
-    await util.writeSettings(guildSettings, localSettings, msg.channel, 'autorole')
+    settings.autorole = null
+    await util.writeSettings(guildSettings, settings, msg.channel, 'autorole')
   } else if (args[1] === 'add') {
-    const localSettings = settings
     if (/\d{18,}/.test(args[2])) {
-      localSettings.autorole = args[2]
+      settings.autorole = args[2]
     } else {
       try {
         settings.autorole = msg.mentions.roles.first().id.toString() || msg.guild.roles.find(n => n.name === args[2]).id
@@ -27,7 +25,7 @@ module.exports.run = async function(msg, settings, lang, guildSettings) {
         msg.channel.send(lang.invalid_args); logger.error(e)
       }
     }
-    await util.writeSettings(guildSettings, localSettings, msg.channel, 'autorole')
+    await util.writeSettings(guildSettings, settings, msg.channel, 'autorole')
   } else {
     if (settings.autorole != null) {
       msg.channel.send(f(lang.autorole_enabled, msg.guild.roles.get(settings.autorole).name))
