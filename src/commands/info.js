abdfff0038e7e29f0dacc96cc73cd01b58f74660
rht0910 <rht0910@rht0910.tk>
2018-09-25 09:24:12 +0900

    more bug fix

--- src/commands/info.js
+++ src/commands/info.js
@@ -5,6 +5,8 @@ const c = require('../config.yml')
 const isWindows = process.platform === 'win32'
 const isTravisBuild = process.argv.includes('--travis-build')
 const s = isTravisBuild ? require('../travis.yml') : require('../secret.yml')
+const logger = require('../logger').getLogger('commands:info')
+const f = require('string-format')
 
 module.exports.name = 'info'
 
@@ -21,21 +23,26 @@ module.exports.run = async function(msg, settings, lang) {
   }
   let owner = ''
   s.owners.forEach(aowner => {
-    if (aowner.tag && aowner.id) owner += `${aowner.tag} (${aowner.id})\n`
+    const user = client.users.get(aowner)
+    if (user.tag && user.id) owner += `${aowner.tag} (${aowner.id})\n`
   })
-  const embed = new Discord.RichEmbed()
-    .setTitle('Bot info')
-    .setTimestamp()
-    .setColor([0,255,0])
-    .addField(lang.info.memory, `${lang.info.memory_max}: ${Math.round(os.totalmem() / 1024 / 1024 * 100) / 100}MB\n${lang.info.memory_usage}: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100}MB\n${lang.info.memory_free}: ${Math.round(os.freemem() / 1024 / 1024 * 100) / 100}MB`)
-    .addField(lang.info.cpu, `${lang.info.threads}: ${os.cpus().length}\n${lang.info.cpu_model}: ${os.cpus()[0].model}\n${lang.info.cpu_speed}: ${os.cpus()[0].speed}`)
-    .addField(lang.info.disk, graph + o1)
-    .addField(lang.info.platform, os.platform)
-    .addField(lang.info.loadavg, loadavg)
-    .addField(lang.info.servers, client.guilds.size)
-    .addField(lang.info.users, client.users.size)
-    .addField(lang.info.createdBy, owner)
-    .setDescription(`[${lang.info.invite}](${invite})\n[${lang.info.source}](${c.github})\n[Discord Bots](https://discordbots.org/bot/456966161079205899)`)
-    .setFooter(`Sent by ${msg.author.tag}`)
-  return msg.channel.send(embed)
+  try {
+    msg.channel.send(new Discord.RichEmbed()
+      .setTitle('Bot info')
+      .setTimestamp()
+      .setColor([0,255,0])
+      .addField(lang.info.memory, `${lang.info.memory_max}: ${Math.round(os.totalmem() / 1024 / 1024 * 100) / 100}MB\n${lang.info.memory_usage}: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100}MB\n${lang.info.memory_free}: ${Math.round(os.freemem() / 1024 / 1024 * 100) / 100}MB`)
+      .addField(lang.info.cpu, `${lang.info.threads}: ${os.cpus().length}\n${lang.info.cpu_model}: ${os.cpus()[0].model}\n${lang.info.cpu_speed}: ${os.cpus()[0].speed}`)
+      .addField(lang.info.disk, graph + o1)
+      .addField(lang.info.platform, os.platform)
+      .addField(lang.info.loadavg, loadavg)
+      .addField(lang.info.servers, client.guilds.size)
+      .addField(lang.info.users, client.users.size)
+      .addField(lang.info.createdBy, owner)
+      .setDescription(`[${lang.info.invite}](${invite})\n[${lang.info.source}](${c.github})\n[Discord Bots](https://discordbots.org/bot/456966161079205899)`)
+      .setFooter(`Sent by ${msg.author.tag}`))
+  } catch(e) {
+    logger.error(f(lang.error, e))
+    msg.channel.send(f(lang.error, e))
+  }
 }
