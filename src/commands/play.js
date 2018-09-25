d15c86066ff5afb1c8ca77425e4c4ee921d2df55
rht0910 <rht0910@rht0910.tk>
2018-09-25 09:18:34 +0900

    bug fix

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,6 +1,6 @@
 const f = require('string-format')
 const isTravisBuild = process.argv.includes('--travis-build')
-const s = require(`${__dirname}/../` + isTravisBuild ? 'travis.yml' : 'secret.yml')
+const s = require(`${__dirname}/../` + (isTravisBuild ? 'travis.yml' : 'secret.yml'))
 
 module.exports.name = 'play'
 
