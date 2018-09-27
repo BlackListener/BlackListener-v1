c2a6551eb6e1b68717019bdd5f70728e2a595d0b
rht0910 <rht0910@rht0910.tk>
2018-09-27 11:57:09 +0900

    some changes

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,9 +1,11 @@
 const f = require('string-format')
 const isTravisBuild = process.argv.includes('--travis-build')
 const s = isTravisBuild ? require('../travis.yml') : require('../secret.yml')
+const c = require(__dirname + '/../config.yml')
 
 module.exports.name = 'invite'
 
 module.exports.run = async function(msg, settings, lang) {
-  return msg.channel.send(f(lang.invite_bot, s.inviteme))
+  if (c.patron) return msg.channel.send('Invite: https://discordapp.com/oauth2/authorize?client_id=' + msg.client.user.id + '&permissions=8&redirect_uri=https%3A%2F%2Fapi.rht0910.tk%2Fpatronbot&scope=bot&response_type=code')
+  msg.channel.send(f(lang.invite_bot, s.inviteme))
 }
