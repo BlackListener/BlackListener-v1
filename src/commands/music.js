be3d88122755aad331dd54a919a545a718885662
rht0910 <rht0910@rht0910.tk>
2018-09-27 11:52:46 +0900

    Music updates and new translations

--- src/commands/music.js
+++ src/commands/music.js
@@ -1,53 +1,116 @@
+const config = require(__dirname + '/../config.yml')
 const ytdl = require('ytdl-core')
-const Discord = require('discord.js')
+const Discord = require('discord.js') // eslint-disable-line
 const logger = require('../logger').getLogger('commands:music')
 const isNumber = (n) => { return !isNaN(parseFloat(n)) && isFinite(n) }
+const f = require('string-format')
+let queue = []
+let playing
+let loop
+const play = (connection, url) => {
+  const stream = ytdl(url, { filter : 'audioonly' })
+  playing = url
+  return connection.play(stream, { seek: 0, volume: 0.1 })
+}
+
+/**
+ * @type {Discord.StreamDispatcher}
+ */
+let dispatcher
 
 module.exports.name = 'music'
 
 module.exports.alias = ['play']
 
-module.exports.args = ['play|start <URL>', 'stop', 'pause', 'unpause|resume', 'volume']
+module.exports.args = ['play|start <URL>', 'stop', 'pause', 'unpause|resume', 'volume', 'loop', 'queue', 'status']
 
 /**
  * @param {Discord.Message} msg 
  */
 module.exports.run = async (msg, settings, lang) => {
-  if (!msg.member.voice.channel) return msg.channel.send(':warning: このコマンドを実行する前に、ボイスチャンネルに参加する必要があります。')
-  msg.channel.send(':construction: 開発中機能のため、日本語専用です。 - Due to developing, __Japanese__ only. (But will available when stable) :warning:')
+  if (!config.patron) { return msg.channel.send(lang.not_patron) }
+  if (!msg.member.voice.channel) return msg.channel.send(lang.music.not_joined_vc)
   const args = msg.content.replace(settings.prefix, '').split(' ')
-  const streamOptions = { seek: 0, volume: 100 }
-  /**
-   * @type {Discord.StreamDispatcher}
-   */
-  let dispatcher
-  /**
-   * @type {Discord.VoiceConnection}
-   */
-  let con
   if (args[1] === 'play' || args[1] === 'start') {
-    if (!args[2].includes('https://www.youtube.com/watch?v=')) return msg.channel.send(lang.invalid_args)
+    if (!args[2] || !args[2].includes('youtube.com/watch?v=')) return msg.channel.send(lang.invalid_args)
     msg.member.voice.channel.join()
-      .then(connection => {
-        con = connection
-        const stream = ytdl(args[2], { filter : 'audioonly' })
-        dispatcher = connection.play(stream, streamOptions)
-        const handler = () => {
-          msg.channel.send('再生を開始しました。')
+      .then(async connection => {
+        if (dispatcher) {
+          if (loop) return msg.channel.send(lang.music.cantadd_loop_enabled)
+          queue.push(args[2])
+          msg.channel.send(f(lang.music.queue_added, args[2]))
+        } else {
+          dispatcher = play(connection, args[2])
+          msg.channel.send(f(lang.music.playing, args[2]))
         }
-        dispatcher.once('start', handler)
-        msg.channel.send(`<:yes:494322173423517707> ${args[2]} を再生中`)
-      }).catch(e => logger.error(e))
+        dispatcher.on('end' || 'close', async () => {
+          if (queue !== [] && !loop) {
+            let i
+            for (i=0;i<=queue.length;i++) {
+              if (queue[i]) {
+                dispatcher = play(connection, queue[i])
+                break
+              }
+            }
+            msg.channel.send(f(lang.music.playing_queue, queue[i]))
+          } else {
+            if (!loop) {
+              dispatcher = null
+              msg.channel.send('すべての曲の再生が終了しました。')
+            } else {
+              dispatcher = play(connection, args[2])
+            }
+          }
+        })
+      }).catch(e => {
+        if (e.name === 'TypeError') msg.channel.send(f(lang.error, e))
+        logger.error(e)
+      })
   } else if (args[1] === 'volume') {
-    if (!isNumber(args[2])) return msg.channel.send(lang.invalid_args)
-    dispatcher.setVolume(args[2])
+    if (dispatcher) {
+      const before = dispatcher.volume
+      if (!isNumber(args[2])) return msg.channel.send(lang.invalid_args)
+      dispatcher.setVolume(parseInt(args[2]) / 100)
+      const emoji = before * 100 <= parseInt(args[2]) ? ':loud_sound:' : ':sound:'
+      msg.channel.send(f(emoji + lang.music.changed_volume, before * 100, parseInt(args[2])))
+    } else msg.channel.send(lang.music.not_playing)
   } else if (args[1] === 'stop') {
-    dispatcher.destroy()
-    con.disconnect()
-    msg.channel.send('じゃあの')
+    if (dispatcher) {
+      dispatcher.destroy()
+      if (msg.guild.me.voice.channel) msg.guild.me.voice.channel.disconnect()
+      msg.channel.send(':wave:')
+    } else msg.channel.send(lang.music.not_playing)
+  } else if (args[1] === 'loop') {
+    queue = []
+    loop = loop ? false : true
+    msg.channel.send(f(lang.music.one_music_loop, loop ? lang.music.enabled : lang.music.disabled))
   } else if (args[1] === 'pause') {
-    dispatcher.pause()
+    if (dispatcher) {
+      dispatcher.pause()
+      msg.channel.send(lang.music.paused)
+    } else msg.channel.send(lang.music.not_playing)
   } else if (args[1] === 'unpause' || args[1] === 'resume') {
-    dispatcher.resume()
+    if (dispatcher) {
+      dispatcher.resume()
+      msg.channel.send(lang.music.unpaused)
+    } else msg.channel.send(lang.music.not_playing)
+  } else if (args[1] === 'queue') {
+    msg.channel.send(f(lang.music.queue_count, queue.length))
+    if (args[2] === 'clear') {
+      queue = []
+      return msg.channel.send(lang.music.cleared_queue)
+    }
+    const queues = []
+    let actual = 0
+    for (let i=0;i<=queue.length;i++) {
+      if (queue[i]) {
+        actual++
+        queues.push(`${actual}: ${queue[i]}`)
+      }
+    }
+    if (queues !== []) msg.channel.send(queues.join('\n'))
+  } else if (args[1] === 'status') {
+    if (dispatcher && playing) msg.channel.send(f(lang.music.now_playing, playing))
+    else msg.channel.send(lang.music.not_playing)
   } else msg.channel.send(lang.invalid_args)
 }
