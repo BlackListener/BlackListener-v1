cd2aa8202173409e8916b3604108ed0a7882c8a9
rht0910 <rht0910@rht0910.tk>
2018-11-02 10:34:37 +0900

    spotted on music command

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,6 +1,7 @@
 const logger = require(__dirname + '/../logger').getLogger('commands:eval', 'lightpurple')
 const f = require('string-format')
 const { Command } = require('../core')
+const argument_parser = require(__dirname + '/../argument_parser')
 
 module.exports = class extends Command {
   constructor() {
@@ -15,10 +16,23 @@ module.exports = class extends Command {
   }
 
   async run(msg, settings, lang) {
+    const args = msg.content.replace(settings.prefix, '').split(' ')
+    const opts = argument_parser(args.slice(1))
     if (msg.content.includes('token')) return msg.channel.send(lang.udonthaveperm)
-    const commandcut = msg.content.substr(`${settings.prefix}eval `.length)
+    const commandcut = args.slice(Object.values(opts).length||0 + 1).join(' ')
     try {
-      const returned = await (eval(`(async () => {${commandcut}})()`))
+      let returned
+      if (opts.run_as) {
+        let Run
+        try {
+          Run = require(__dirname + '/' + opts.run_as)
+        } catch(e) {
+          throw new ReferenceError('Not found specified command.')
+        }
+        returned = await (new Run())._eval(commandcut)
+      } else {
+        returned = await (eval(`(async () => {${commandcut}})()`))
+      }
       logger.info(`Eval by ${msg.author.tag} (${msg.author.id}), result: ${returned}`)
       msg.channel.send(`:ok_hand: (${returned})`)
     } catch (e) {
--- src/commands/music.js
+++ src/commands/music.js
@@ -7,9 +7,9 @@ const { Command } = require('../core')
 const YouTube = require('youtube-node')
 const youtube = new YouTube()
 const util = require('util')
-let queue = {}
+const queue = {}
 let playing
-let loop = {}
+const loop = {}
 const play = (connection, url, msg, lang) => {
   try {
     const stream = ytdl(url, { filter : 'audioonly' })
@@ -45,6 +45,7 @@ module.exports = class extends Command {
     if (!config.patron) { return msg.channel.send(lang.not_patron) }
     if (!msg.member.voiceChannel) return msg.channel.send(lang.music.not_joined_vc)
     const args = msg.content.replace(settings.prefix, '').split(' ')
+    loop[msg.guild.id] = {} // need initialize!
     if (args[1] === 'join') {
       msg.member.voiceChannel.join().then(vc => msg.channel.send(f(lang.music.joined_vc, vc.channel.name))).catch(e => logger.error(e))
     } else if (args[1] === 'play' || args[1] === 'start') {
@@ -59,7 +60,8 @@ module.exports = class extends Command {
       msg.member.voiceChannel.join()
         .then(async connection => {
           if (msg.guild.voiceConnection && msg.guild.voiceConnection.dispatcher && !msg.guild.voiceConnection.dispatcher.destroyed) {
-            if (loop) return msg.channel.send(lang.music.cantadd_loop_enabled)
+            if (loop[msg.guild.id].enabled) return msg.channel.send(lang.music.cantadd_loop_enabled)
+            if (!queue[msg.guild.id]) queue[msg.guild.id] = []
             queue[msg.guild.id].push(args[2])
             msg.channel.send(f(lang.music.queue_added, args[2]))
             if (keyword) msg.channel.send('Search keyword: ' + keyword)
@@ -70,23 +72,28 @@ module.exports = class extends Command {
           }
           if (msg.deletable) msg.delete()
           const endHandler = async q => {
-            q = q[msg.guild.id]
-            if (q.length && !loop) { // if queue is *not* empty and not enabled loop
+            if (q && (q ? q.length : false) && !loop[msg.guild.id].enabled) { // if queue is *not* empty and not enabled loop
               play(connection, q[0], msg, lang)
               msg.channel.send(f(lang.music.playing_queue, q[0]))
-              q = q.slice(1)
+              queue[msg.guild.id] = q[msg.guild.id].slice(1)
               register(q) //eslint-disable-line
             } else { // if queue is empty or enabled looping
-              if (!loop) { // loop is disabled
+              if (!loop[msg.guild.id].enabled && !loop[msg.guild.id].every) { // loop is disabled
                 msg.channel.send(lang.music.ended)
               } else { // loop is enabled
-                play(connection, args[2], msg, lang)
-                register(q) //eslint-disable-line
+                if (loop[msg.guild.id].every) {
+                  const seconds = parseInt(loop[msg.guild.id].every.replace(/\D{1,}/gm, '')) * 60
+                  setTimeout(() => {
+                    play(connection, args[2], msg, lang)
+                  }, seconds * 1000)
+                } else {
+                  play(connection, args[2], msg, lang)
+                  register(q) //eslint-disable-line
+                }
               }
             }
           }
           const register = q => {
-            queue = q
             msg.guild.voiceConnection.dispatcher.once('end', () => endHandler(q))
             msg.guild.voiceConnection.dispatcher.once('close', () => endHandler(q))
           }
@@ -99,17 +106,38 @@ module.exports = class extends Command {
         msg.guild.voiceConnection.dispatcher.setVolume(parseInt(args[2]) / 1000)
         const emoji = before * 1000 <= parseInt(args[2]) ? ':loud_sound:' : ':sound:'
         msg.channel.send(f(emoji + lang.music.changed_volume, before * 100, parseInt(args[2]))) // oldvalue: 100
-      } else msg.channel.send(lang.music.not_playing)
+      } else return msg.channel.send(lang.music.not_playing)
+    } else if (args[1] === 'skip') {
+      if (!loop[msg.guild.id].enabled && msg.guild.voiceConnection && msg.guild.voiceConnection.dispatcher && !msg.guild.voiceConnection.dispatcher.destroyed) {
+        msg.guild.voiceConnection.dispatcher.end() // Just stop the music and send end event, and play queue.
+      } else return msg.channel.send(lang.music.not_playing + ' ...or enabled loop.')
     } else if (args[1] === 'stop') {
+      queue[msg.guild.id] = []
       if (msg.guild.voiceConnection && msg.guild.voiceConnection.dispatcher && !msg.guild.voiceConnection.dispatcher.destroyed) {
-        msg.guild.voiceConnection.dispatcher.destroy()
+        msg.guild.voiceConnection.dispatcher.end()
+        msg.guild.voiceConnection.dispatcher.destroyed = true
         if (msg.guild.me.voiceChannel && msg.guild.me.voiceChannel.connection) msg.guild.me.voiceChannel.connection.disconnect()
         msg.channel.send(':wave:')
-      } else msg.channel.send(lang.music.not_playing)
+      } else return msg.channel.send(lang.music.not_playing)
     } else if (args[1] === 'loop') {
-      queue[msg.guild.id] = []
-      loop = loop ? false : true
-      msg.channel.send(f(lang.music.one_music_loop, loop ? lang.music.enabled : lang.music.disabled))
+      Object.assign({[msg.guild.id]: []}, queue)
+      //queue.push({[msg.guild.id]: []})
+      if (args[2] === 'disable') {
+        loop[msg.guild.id].every = null
+        loop[msg.guild.id].enabled = false
+        msg.channel.send(lang.music.disabled_looping)
+      } else {
+        if (args[2] === 'every') {
+          if (args[3] === 'clear') return loop[msg.guild.id].every = null
+          if (!/\d{1,}m/gm.test(args[3])) return msg.channel.send(lang.invalid_args)
+          loop[msg.guild.id].enabled = true
+          loop[msg.guild.id].every = args[3]
+          msg.channel.send(f(lang.music.loop_every, args[3]))
+        } else {
+          loop[msg.guild.id].enabled = loop[msg.guild.id].enabled ? false : true
+          msg.channel.send(f(lang.music.one_music_loop, loop ? lang.music.enabled : lang.music.disabled))
+        }
+      }
     } else if (args[1] === 'pause') {
       if (msg.guild.voiceConnection && msg.guild.voiceConnection.dispatcher && !msg.guild.voiceConnection.dispatcher.destroyed) {
         msg.guild.voiceConnection.dispatcher.pause()
@@ -126,9 +154,9 @@ module.exports = class extends Command {
         queue[msg.guild.id] = []
         return msg.channel.send(lang.music.cleared_queue)
       } else if (args[2] === 'reactivate') {
-        return msg.channel.send('You need to type this: ```\n<prefix>music stop\n<prefix>music play https://www.youtube.com/watch?v=jhFDyDgMVUI\n```')
+        return msg.channel.send(f('You need to type this: ```\n{0}music stop\n{0}music play https://www.youtube.com/watch?v=jhFDyDgMVUI\n```', settings.prefix))
       }
-      const queues = queue[msg.guild.id].map((e, i) => `${i}: ${e}`)
+      const queues = queue[msg.guild.id] ? queue[msg.guild.id].map((e, i) => `${i}: ${e}`) : []
       if (queues.length) msg.channel.send(queues.join('\n'))
     } else if (args[1] === 'status') {
       if (msg.guild.voiceConnection && msg.guild.voiceConnection.dispatcher && !msg.guild.voiceConnection.dispatcher.destroyed && playing) msg.channel.send(f(lang.music.now_playing, playing))
