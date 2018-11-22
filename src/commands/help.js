20d91ef93943018f7a3d19ef84492c325366ada6
yuta0801 <yuta20030801@icloud.com>
2018-11-22 11:49:09 +0900

    Incorporate lang/commands/*.json into lang/*.json

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,7 +2,6 @@ const f = require('string-format')
 const Discord = require('discord.js')
 const c = require(__dirname + '/../config.yml')
 const util = require(__dirname + '/../util')
-const languages = require(__dirname + '/../language')
 const { Command } = require('../core')
 
 module.exports = class extends Command {
@@ -18,12 +17,11 @@ module.exports = class extends Command {
   async run(msg, settings, lang, args) {
     const {commands} = require(__dirname + '/../commands')
     if (args[1]) {
-      const langcommands = languages.commands[settings.language]
       if (!commands[args[1]]) return msg.channel.send(f(lang.no_command, args[1]))
       const embed = new Discord.RichEmbed()
         .setTitle('About this command')
         .setDescription(
-          (langcommands[args[1]] || ' - Not available information - ')
+          (lang.commands[args[1]] || ' - Not available information - ')
           + `\n\nUsage: ${settings.prefix}${args[1]} ${await util.exists(`${__dirname}/${args[1]}.js`) ? (new (require(`${__dirname}/${args[1]}`))().args ? new (require(`${__dirname}/${args[1]}`))().args.join('\n') : '') : '<?>'}`
           + `\nAlias: ${await util.exists(`${__dirname}/${args[1]}.js`) ? (new (require(`${__dirname}/${args[1]}`))().alias ? new (require(`${__dirname}/${args[1]}`))().alias.join('\n') : lang.no) : '?'}`
           + `\n\nAlso see: http://docs.blacklistener.tk/ja/latest/commands/${args[1]}.html`)
@@ -33,20 +31,20 @@ module.exports = class extends Command {
     }
     const prefix = settings.prefix
     const embed = new Discord.RichEmbed()
-      .setTitle(f(lang.commands.title, c.version))
+      .setTitle(f(lang.help.title, c.version))
       .setTimestamp()
       .setColor([0,255,0])
-      .addField(`${prefix}setprefix`, lang.commands.setprefix)
-      .addField(`${prefix}ban [<User> <Reason> <Probe>] | ${prefix}unban`, `${lang.commands.ban} | ${lang.commands.unban}`)
-      .addField(`${prefix}language`, lang.commands.language)
-      .addField(`${prefix}setnotifyrep | ${prefix}setbanrep`, `${lang.commands.setnotifyrep} | ${lang.commands.setbanrep}`)
-      .addField(`${prefix}antispam`, lang.commands.antispam)
-      .addField(`${prefix}dump [guilds|users|channels|emojis|messages] [messages:delete/size]`, lang.commands.dump)
-      .addField(`${prefix}invite`, lang.commands.invite)
-      .addField(`${prefix}role <role> [user] __/__ ${prefix}autorole [add/remove] <role>`, `${lang.commands.role}\n${lang.commands.autorole}`)
-      .addField(`${prefix}image [anime|custom] [subreddit]`, lang.commands.image)
+      .addField(`${prefix}setprefix`, lang.help.setprefix)
+      .addField(`${prefix}ban [<User> <Reason> <Probe>] | ${prefix}unban`, `${lang.help.ban} | ${lang.help.unban}`)
+      .addField(`${prefix}language`, lang.help.language)
+      .addField(`${prefix}setnotifyrep | ${prefix}setbanrep`, `${lang.help.setnotifyrep} | ${lang.help.setbanrep}`)
+      .addField(`${prefix}antispam`, lang.help.antispam)
+      .addField(`${prefix}dump [guilds|users|channels|emojis|messages] [messages:delete/size]`, lang.help.dump)
+      .addField(`${prefix}invite`, lang.help.invite)
+      .addField(`${prefix}role <role> [user] __/__ ${prefix}autorole [add/remove] <role>`, `${lang.help.role}\n${lang.help.autorole}`)
+      .addField(`${prefix}image [anime|custom] [subreddit]`, lang.help.image)
       .addField(`${prefix}lookup <User>`, lang.lookup.description)
-      .addField(lang.commands.others, lang.commands.athere)
+      .addField(lang.help.others, lang.help.athere)
     return msg.channel.send(embed)
   }
 }
--- src/language.js
+++ src/language.js
@@ -1,8 +1,4 @@
 module.exports = {
   en: require(__dirname + '/lang/en.json'),
   ja: require(__dirname + '/lang/ja.json'),
-  commands: {
-    en: require(__dirname + '/lang/commands/en.json'),
-    ja: require(__dirname + '/lang/commands/ja.json'),
-  },
 }
