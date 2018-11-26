df6f0471c359a4b14ba1a84bbfcee9e8e8a2fd25
yuta0801 <yuta20030801@icloud.com>
2018-11-26 15:38:36 +0900

    Reconstruct setnick command

---
+++ klasa/commands/nickname.js
@@ -0,0 +1,22 @@
+const { Command } = require('klasa')
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      subcommands: true,
+      usage: '<set|reset> <Member:member> (NewName:str)',
+      aliases: ['nick'],
+      permissionLevel: 6,
+    })
+  }
+
+  set(msg, [member, name]) {
+    if (!name) return msg.channel.send(lang._invalid_args)
+    member.setNickname(name)
+  }
+
+  reset(msg, [member]) {
+    member.setNickname(null)
+    return msg.channel.send(':ok_hand:')
+  }
+}
