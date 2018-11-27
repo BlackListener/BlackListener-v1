0db11a5792336b2fc9a08611dadefed09536d931
yuta0801 <yuta20030801@icloud.com>
2018-11-27 13:06:53 +0900

    Fix git command

--- klasa/commands/git.js
+++ klasa/commands/git.js
@@ -10,12 +10,12 @@ module.exports = class extends Command {
 
   async run(msg) {
     const hash = await git.revparse(['HEAD'])
-    const message = await msg.sendLocale('COMMAND_GIT_COMMIT', [hash]) + 'Checking for status...'
+    await msg.sendLocale('COMMAND_GIT_COMMIT', [hash]) + 'Checking for status...'
     await git.fetch()
     const status = await git.status()
-    message.edit(msg.language.get('COMMAND_GIT_COMMIT', hash)
+    msg.sendLocale('COMMAND_GIT_COMMIT', [hash
       + (status.behind === 0 ? '(✔ Running latest version) ' : `(${status.behind} commit(s) behind) `)
       + (status.isClean() ? '' : '[⚠ Pending commit]')
-      + (status.ahead === 0 ? '' : '[⚠ Pending push]'))
+      + (status.ahead === 0 ? '' : '[⚠ Pending push]')])
   }
 }
