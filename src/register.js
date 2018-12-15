f42a26d71b728027ed15811660b396c48ad4206f
acrylic-style <rht0910@rht0910.tk>
2018-12-16 05:14:38 +0900

    changed messages, bug fixes, improves.

--- src/register.js
+++ src/register.js
@@ -88,7 +88,7 @@ module.exports = function(client) {
   client.on('disconnect', () => {
     logger.info(`Disconnected from Websocket (${count}ms).`)
     if (count === 0)
-      logger.fatal('May wrong your bot token, Is bot token has changed or Base64 encoded?')
+      logger.fatal('May wrong your bot token, Is bot token has changed?')
         .info('Or are you attempted remote shutdown?')
     process.exit()
   })
@@ -98,7 +98,7 @@ module.exports = function(client) {
   })
 
   client.on('error', (e) => {
-    const err = typeof e == Object ? util.inspect(e) : e
+    const err = typeof e == 'object' ? util.inspect(e) : e
     logger.fatal('Something went wrong: ' + err)
     logger.fatal(e.stack || e)
   })
@@ -115,9 +115,7 @@ module.exports = function(client) {
     }
     if (client.readyAt && c.errors_channel) client.channels.get(c.errors_channel).send(codeblock(report))
       .then(() => logger.info('Error report has been sent!'))
-    fs.writeFile(file, report, 'utf8').then(() => {
-      logger.info(`Error Report has been writed to ${file}`)
-    })
+    fs.writeFile(file, report, 'utf8').then(() => logger.info(`Error Report has been writed to ${file}`))
     errors++
   })
 
@@ -135,12 +133,13 @@ module.exports = function(client) {
     if (msg === 'heartbeat') process.send('ping')
   })
 
-  client.on('rateLimit', (info, method) => {
+  client.on('rateLimit', info => {
+    if (info.limit === 1) return
+    setTimeout(() => logger.info('Rate limit is no longer active.'), info.limit*1000)
     logger.error('==============================')
       .error('      Got rate limiting!      ')
       .error(` -> ${info.limit} seconds remaining`)
-      .error(` Detected rate limit while processing '${method}' method.`)
-      .error(` Rate limit information: ${JSON.stringify(info)} `)
+      .error(` Rate limit information: ${JSON.stringify(info)}`)
       .error('==============================')
   })
 
